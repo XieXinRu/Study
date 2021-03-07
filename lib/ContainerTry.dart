1d61171ab22e68617a6020385f9a1b78516d852b
XieXinRu <10936009@ntub.edu.tw>
2021-03-07 17:21:15 +0800

    container一些功能

--- lib/ContainerTry.dart
+++ lib/ContainerTry.dart
@@ -11,13 +11,19 @@ class  ContainerTry extends StatelessWidget{
       body:
       //容器Container-基础属性：width，height，color，child
       Center(
-           child: Container(
-           padding: EdgeInsets.all(30), //留白
-           color: Colors.blue,
-           width: 200,
-           height: 200,
-           child: Text("Hello Container ",style:TextStyle(fontSize: 20,color: Colors.white)),
-        )
+          child: Container(
+              padding: EdgeInsets.all(30), //留白
+              margin: EdgeInsets.only(left: 150,top: 0,right: 0,bottom: 0), //邊界
+              transform: Matrix4.rotationZ(0.5), //旋轉
+              color: Colors.blue,
+              width: 200,
+              height: 200,
+              //  child: Text("Hello Container ",style:TextStyle(fontSize: 20,color: Colors.white)),
+              child:Image( //匯入圖片
+                  image: AssetImage("assets/images/main.png"),
+                    width: 200.0,
+              )
+          )
       )
     );
   }
